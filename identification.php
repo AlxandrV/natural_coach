4143c3754a820a53e7f8ceca15f3052254645c5f
AlxandrV <a.vigneaud@gmail.com>
2020-06-22 14:46:47 +0200

    Add - table sql and while for list excursion

--- identification.php
+++ identification.php
@@ -1,13 +1,25 @@
 <?php
 session_start();
 try{
-    $bdd = new PDO('mysql:host=localhost;dbname=alexandrev_BDD_ACS;charset=utf8', 'vagrantdb', 'vagrantdb',
+    $bdd = new PDO('mysql:host=localhost;dbname=natural_coach;charset=utf8', 'vagrantdb', 'vagrantdb',
     array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
 catch (Exeption $e){
     die('Erreur : ' . $e -> getmessage());
 }
 
-if(isset($_POST['login']) && isset($_POST['password'])){
-
+if(isset($_POST['login_membre']) && isset($_POST['password_membre'])){
+    $login = $_POST['login_membre'];
+    $password = $_POST['password_membre'];
+    $check_login = $bdd -> prepare('SELECT * FROM membre WHERE login = :login AND password = :password');
+    $check_login -> execute(array('login' => $login, 'password' => $password));
+    while($donnee = $check_login -> fetch()){
+        if(isset($donnee)){
+            $_SESSION['membre'] = true;
+            $_SESSION['nom_membre'] = $donnee['nom'];
+            $_SESSION['prenom_membre'] = $donnee['prenom'];
+        }
+    }  
+    $check_login -> closeCursor();  
 }
+header('Location: index.php');
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+//session_destroy();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,15 +10,34 @@ session_start();
     <title>Natural Coach</title>
 </head>
 <body>
-<form action="identification.php" method="POST">
-    <input type="text" name="login" id="login">
-    <input type="text" name="password" id="password">
-    <input type="submit" value="Valider">
-</form>
 <?php
+if(!isset($_SESSION['membre'])){
+?>
+    <form action="identification.php" method="POST">
+        <input type="text" name="login_membre" id="login" placeholder="login">
+        <input type="text" name="password_membre" id="password" placeholder="password">
+        <input type="submit" value="Valider">
+    </form>
+<?php
+}
+else{
+    echo $_SESSION['nom_membre'] . '<br/>' . $_SESSION['prenom_membre'];
 
+    try{
+        $bdd = new PDO('mysql:host=localhost;dbname=natural_coach;charset=utf8', 'vagrantdb', 'vagrantdb',
+        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    }
+    catch (Exeption $e){
+        die('Erreur : ' . $e -> getmessage());
+    }
 
-
+    echo '<form action="incription.php" method="POST">';
+    $excursion = $bdd -> query('SELECT * FROM  excursion');
+    while($donnees = $excursion -> fetch()){
+        echo '<p>Nom : ' . $donnees['nom'] . ', date départ : ' . $donnees['date_depart'] . ', date retour : ' . $donnees['date_retour'] . ', depart : ' . $donnees['point_depart'] . ', arrivée : ' . $donnees['point_arrivee'] .', tarif : ' . $donnees['tarif'] . '.</p><button type="submit" name"' . $donnees['id'] . '">S\'inscrire</button>';
+    }
+    echo '</form>';
+}
 ?>  
 </body>
 </html>
