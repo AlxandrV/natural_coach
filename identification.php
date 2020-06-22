64bfcd55b0390ddf0c20b46400094723ee0ba867
AlxandrV <a.vigneaud@gmail.com>
2020-06-22 16:15:55 +0200

    Add - identification for admin and space work admin

--- identification.php
+++ identification.php
@@ -8,16 +8,14 @@ catch (Exeption $e){
     die('Erreur : ' . $e -> getmessage());
 }
 
-if(isset($_POST['login_membre']) && isset($_POST['password_membre'])){
-    $login = $_POST['login_membre'];
-    $password = $_POST['password_membre'];
-    $check_login = $bdd -> prepare('SELECT * FROM membre WHERE login = :login AND password = :password');
+if(isset($_POST['login_admin']) && isset($_POST['password_admin'])){
+    $login = $_POST['login_admin'];
+    $password = $_POST['password_admin'];
+    $check_login = $bdd -> prepare('SELECT * FROM admin WHERE login = :login AND password = :password');
     $check_login -> execute(array('login' => $login, 'password' => $password));
     while($donnee = $check_login -> fetch()){
         if(isset($donnee)){
-            $_SESSION['membre'] = true;
-            $_SESSION['nom_membre'] = $donnee['nom'];
-            $_SESSION['prenom_membre'] = $donnee['prenom'];
+            $_SESSION['id_admin'] = $donnee['id'];
         }
     }  
     $check_login -> closeCursor();  
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-//session_destroy();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,33 +10,44 @@ session_start();
 </head>
 <body>
 <?php
-if(!isset($_SESSION['membre'])){
+// Connexion BDD
+try{
+    $bdd = new PDO('mysql:host=localhost;dbname=natural_coach;charset=utf8', 'vagrantdb', 'vagrantdb',
+    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch (Exeption $e){
+    die('Erreur : ' . $e -> getmessage());
+}
+
+// Login pour admin
+if(!isset($_SESSION['id_membre'])){
 ?>
     <form action="identification.php" method="POST">
-        <input type="text" name="login_membre" id="login" placeholder="login">
-        <input type="text" name="password_membre" id="password" placeholder="password">
+        <input type="text" name="login_admin" id="login" placeholder="login">
+        <input type="text" name="password_admin" id="password" placeholder="password">
         <input type="submit" value="Valider">
     </form>
 <?php
-}
-else{
-    echo $_SESSION['nom_membre'] . '<br/>' . $_SESSION['prenom_membre'];
-
-    try{
-        $bdd = new PDO('mysql:host=localhost;dbname=natural_coach;charset=utf8', 'vagrantdb', 'vagrantdb',
-        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-    }
-    catch (Exeption $e){
-        die('Erreur : ' . $e -> getmessage());
-    }
-
-    echo '<form action="incription.php" method="POST">';
     $excursion = $bdd -> query('SELECT * FROM  excursion');
     while($donnees = $excursion -> fetch()){
-        echo '<p>Nom : ' . $donnees['nom'] . ', date départ : ' . $donnees['date_depart'] . ', date retour : ' . $donnees['date_retour'] . ', depart : ' . $donnees['point_depart'] . ', arrivée : ' . $donnees['point_arrivee'] .', tarif : ' . $donnees['tarif'] . '.</p><button type="submit" name"' . $donnees['id'] . '">S\'inscrire</button>';
+        echo '<p>Nom : ' . $donnees['nom'] . ', date départ : ' . $donnees['date_depart'] . ', date retour : ' . $donnees['date_retour'] . ', depart : ' . $donnees['point_depart'] . ', arrivée : ' . $donnees['point_arrivee'] .', tarif : ' . $donnees['tarif'] . '.</p>';
     }
-    echo '</form>';
 }
+else{
+    $excursion = $bdd -> query('SELECT * FROM  excursion');
+    while($donnees = $excursion -> fetch()){
+        echo '<p>Nom : ' . $donnees['nom'] . ', date départ : ' . $donnees['date_depart'] . ', date retour : ' . $donnees['date_retour'] . ', depart : ' . $donnees['point_depart'] . ', arrivée : ' . $donnees['point_arrivee'] .', tarif : ' . $donnees['tarif'] . '.</p>';
+    }
+?>
+    <form action="ajout.php" method="POST" novaldate>
+        <p>jouter une nouvelle excursion :</p>
+        <p>Nom de la randonnée <input type="text" name="nom_excursion"></input>, date de départ <input type="date" name="date_depart" required></input>, date de retour <input type="date" name="date_retour" required></input>, point de départ <input type="text" name="point_depart" required></input>
+        , point d'arrivée <input type="text" name="point_arrivee" required></input>, région de départ <input type="text" name="region_depart" required></input>, région d'arrivée <input type="text" name="region_arrivee" required></input>, tarif <input type="number" name="tarif" min="0" step="any" required></input></p>
+        <input type="submit" value="Créer">
+    </form>
+<?php
+}
+$excursion -> closeCursor();
 ?>  
 </body>
 </html>
